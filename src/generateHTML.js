6a0e8cb4df24fadd1eb7c0c7fc4611a4504304a2
Cosdaman <Cosdaguy@gmail.com>
2021-11-17 12:36:56 -0800

    style is finalized

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -17,7 +17,7 @@ let htmlfileOpen = `<!DOCTYPE html>
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
-<body>
+<body style="background-color: #96E072;">
 <nav class="navbar navbar-dark bg-dark d-flex">
     <div class="container-fluid justify-content-center">
         <span class="navbar-brand mb-0 h1">Team Profile</span>
@@ -58,38 +58,38 @@ function generateHtml(staffArr) {
             htmlfileOpen += `
             <div id="manager" class="row justify-content-center">
             <h1 class="col-12" style="text-align: center;">Manager</h1>
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 18rem;background-color: #273C2C; color: #FFFFFF;">
                 <div class="card-body">
                     <h5 class="card-title">${element.name}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${element.getRole()}</h6>
+                    <h6 class="card-subtitle mb-2 fw-bolder">${element.getRole()}</h6>
                     <p class="card-text">ID: ${element.id}</p>
                     <p class="card-text">Office: ${element.officeNum}</p>
-                    <p class="card-text">Email: <a href="mailto:${element.email}" target="_blank">${element.email}</a></p>
+                    <p class="card-text">Email: <a class="link-light" href="mailto:${element.email}" target="_blank">${element.email}</a></p>
             </div>
         </div>
     </div>
 `
         } else if (element instanceof Engineer) {
             engineerContent += `
-<div class="card col-3" style="width: 18rem;">
+<div class="card col-3" style="width: 18rem;background-color: #3E8914; color: #FFFFFF;">
 <div class="card-body">
     <h5 class="card-title">${element.name}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">${element.getRole()}</h6>
+    <h6 class="card-subtitle mb-2 fw-bolder">${element.getRole()}</h6>
     <p class="card-text">ID: ${element.id}</p>
-    <p class="card-text">Github: <a href="https://github.com/${element.github}" target="_blank">${element.github}</a></p>
-    <p class="card-text">Email: <a href="mailto:${element.email}" class="card-link">${element.email}</a></p>
+    <p class="card-text">Github: <a class="link-light" href="https://github.com/${element.github}" target="_blank">${element.github}</a></p>
+    <p class="card-text">Email: <a class="link-light" href="mailto:${element.email}" class="card-link">${element.email}</a></p>
 </div>
 </div>
 `
         } else if (element instanceof Intern) {
             internContent += `
-<div class="card col-3" style="width: 18rem;">
+<div class="card col-3" style="width: 18rem;background-color: #3DA35D; color: #FFFFFF;">
             <div class="card-body">
                 <h5 class="card-title">${element.name}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">${element.getRole()}</h6>
+                <h6 class="card-subtitle mb-2 fw-bolder">${element.getRole()}</h6>
                 <p class="card-text">ID: ${element.id}</p>
                 <p class="card-text">School: ${element.school}</p>
-                <p class="card-text">Email: <a href="mailto:${element.email}" class="card-link">${element.email}</a></p>
+                <p class="card-text">Email: <a class="link-light" href="mailto:${element.email}" class="card-link">${element.email}</a></p>
             </div>
             </div>
             `
